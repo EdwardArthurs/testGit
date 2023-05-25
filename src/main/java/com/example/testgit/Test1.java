2b5e5839f4f1edaa217f47078d1d8a9cc5eb4402
admin <gnuser@126.com>
2023-05-25 20:58:55 +0800

    1.4

--- src/main/java/com/example/testgit/Test1.java
+++ src/main/java/com/example/testgit/Test1.java
@@ -3,5 +3,8 @@ package com.example.testgit;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("2");
+
+        System.out.println("3");
     }
 }
